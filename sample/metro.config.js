3f42731482865973632c1f7fadda62c98202f9f3
Andrew Tran <andrew.tran@infillion.com>
2023-12-08 15:23:32 -0800

    fix: sample app metro errors
    Remove unnecessary babel plugin declaration.
    Add metro setup from sample app before faulty 0.72 upgrade.
    Update sample app devDependencies for 0.72, per React Native upgrade guide.

--- sample/metro.config.js
+++ sample/metro.config.js
@@ -1,4 +1,11 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+const root = path.resolve(__dirname, '..');
+const pak = require('../package.json');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const modules = Object.keys({
+  ...pak.peerDependencies,
+});
 
 /**
  * Metro configuration
@@ -7,6 +14,17 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
+  projectRoot: __dirname,
+  resolver: {
+    blacklistRE: exclusionList(
+      modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))
+    ),
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'], //add here
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -15,9 +33,7 @@ const config = {
       },
     }),
   },
-  resolver: {
-    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'], //add here
-  },
+  watchFolders: [root],
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
